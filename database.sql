166b06a6d8ae9dd889f2c82d7e94a65321bbd254
Tuet Alexandre <tuet.alex@gmail.com>
2018-10-09 15:30:46 +0200

    modif : bdd vide

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  mar. 09 oct. 2018 à 13:43
+-- Généré le :  mar. 09 oct. 2018 à 15:27
 -- Version du serveur :  10.1.26-MariaDB
 -- Version de PHP :  7.1.9
 
@@ -36,23 +36,6 @@ CREATE TABLE `devoir` (
   `serveur_discord_id` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `devoir`
---
-
-INSERT INTO `devoir` (`devoir_id`, `devoir_matiere`, `devoir_contenu`, `devoir_date`, `serveur_discord_id`) VALUES
-(1, 'Anglais', 'Faire l\'exercice sur 7Speaking', '2018-11-01', '498069345713258506'),
-(2, 'Linux', 'Faire TP', '2018-10-24', '498069345713258506'),
-(3, 'Français', 'Exo', '2018-11-01', '498069345713258506'),
-(4, 'C#', 'Faire exercice', '2018-11-20', '498069345713258506'),
-(5, 'UML', 'Faire le diagramme de Classe', '2018-10-11', '498069345713258506'),
-(7, 'PHP (Symfony)', 'Faire l\'installation du framework', '2018-11-20', '498069345713258506'),
-(30, 'SQL', 'Faire la méthode Merise du dernier exo', '2018-12-18', '498069345713258506'),
-(37, 'Algorithme', 'Refaire l\'exercice 2 du DS 1 sur e-campus', '2018-10-08', '498069345713258506'),
-(38, 'SQL', 'Faire le diaporama avec les def du SQL Server', '2018-10-08', '498069345713258506'),
-(41, 'Français', 'Igiuerhfiu iyqgiergh', '2018-12-12', '498069345713258506'),
-(45, 'UML', 'Finir exercice 1', '2018-10-10', '498069345713258506'),
-(46, 'Test', 'Test notification', '2018-10-10', '498069345713258506');
 
 -- --------------------------------------------------------
 
@@ -71,14 +54,6 @@ CREATE TABLE `serveur` (
   `serveur_channel_notif` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `serveur`
---
-
-INSERT INTO `serveur` (`serveur_discord_id`, `serveur_nom`, `serveur_prefix`, `serveur_date_join`, `serveur_channel_name`, `serveur_role_admin_id`, `serveur_role_notif_id`, `serveur_channel_notif`) VALUES
-('358623719914209301', 'WBot', '!', '2018-10-07 11:09:23', 'devoirs', NULL, NULL, '498421683766689813'),
-('413078524128133131', 'test', '!', '2018-10-07 12:30:35', NULL, NULL, NULL, '460017836245385216'),
-('498069345713258506', 'BOT', '!', '2018-10-06 12:00:00', 'devoirs', NULL, NULL, '498420362170597387');
 
 --
 -- Index pour les tables déchargées
@@ -105,7 +80,7 @@ ALTER TABLE `serveur`
 -- AUTO_INCREMENT pour la table `devoir`
 --
 ALTER TABLE `devoir`
-  MODIFY `devoir_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `devoir_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- Contraintes pour les tables déchargées
