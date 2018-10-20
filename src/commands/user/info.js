1500c02e08eaaf1b6a75820718225df5ea1342c0
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-10-20 13:22:37 +0200

    ajout: stats nombre de devoirs totaux & par serveur

--- src/commands/user/info.js
+++ src/commands/user/info.js
@@ -18,34 +18,46 @@ const packageJson = require('../../../package.json')
  * affiche diverses informations sur le bot
  */
 module.exports.run = async (wbot, message, args) => {
-  // Récupération et formatage de l'uptime
-  const duration = moment.duration(wbot.uptime).format(' D [jour], H [heure] et m [minute], s [s]')
+  /**
+   * Récupération nombre de devoirs
+   * (- sur le serveur)
+   * (- tout serveurs confondus)
+   */
+  wbot.database.query(`SELECT COUNT(*) AS total, SUM(case when serveur_discord_id = '${message.guild.id}' then 1 else 0 end) AS server FROM devoir`, function (err, rows, fields) {
+    if (err) wbot.logger.log(err, 'error')
 
-  var desc = ''
-  desc += '**`' + beautify('Version du bot') + '`** : v' + packageJson.version + '\n'
-  desc += '**`' + beautify('Version discordjs') + '`** : v' + packageJson.dependencies['discord.js'] + '\n'
-  desc += '**`' + beautify('Version nodejs') + '`** : ' + process.version + '\n'
-  desc += '**`' + beautify('Code source') + '`** : [Woosy/WBot](https://github.com/Woosy/WBot) \n'
-  desc += '---------------------------\n'
-  desc += '**`' + beautify('Uptime') + '`** : ' + duration + '\n'
-  desc += '**`' + beautify('Memory usage') + '`** : ' + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB\n'
-  desc += '---------------------------\n'
-  desc += '**`' + beautify('Serveurs') + '`** : ' + wbot.guilds.size + '\n'
-  desc += '**`' + beautify('Channels') + '`** : ' + wbot.channels.size + '\n'
-  desc += '**`' + beautify('Utilisateurs') + '`** : ' + wbot.users.size + '\n'
-  // desc += '**`' + beautify('Créateurs') + '`** : Arthur / Woosy#4710 & Alexandre#2086\n'
+    // Récupération et formatage de l'uptime
+    const duration = moment.duration(wbot.uptime).format(' D [jour], H [heure] et m [minute], s [s]')
+    const nbDevoirs = rows[0].server || 0
 
-  // Construction de la réponse
-  const embed = new Discord.RichEmbed()
-    .setColor('#3586ff')
-    .setAuthor(wbot.user.username, wbot.user.avatarURL)
-    .setTitle('Informations :')
-    .setDescription(desc)
-    .setFooter(message.author.username, message.author.avatarURL)
-    .setTimestamp()
+    var desc = ''
+    desc += '**`' + beautify('Version du bot') + '`** : v' + packageJson.version + '\n'
+    desc += '**`' + beautify('Version discordjs') + '`** : v' + packageJson.dependencies['discord.js'] + '\n'
+    desc += '**`' + beautify('Version nodejs') + '`** : ' + process.version + '\n'
+    desc += '**`' + beautify('Code source') + '`** : [Woosy/WBot](https://github.com/Woosy/WBot) \n'
+    desc += '---------------------------\n'
+    desc += '**`' + beautify('Uptime') + '`** : ' + duration + '\n'
+    desc += '**`' + beautify('Memory usage') + '`** : ' + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB\n'
+    desc += '---------------------------\n'
+    desc += '**`' + beautify('Serveurs') + '`** : ' + wbot.guilds.size + '\n'
+    desc += '**`' + beautify('Channels') + '`** : ' + wbot.channels.size + '\n'
+    desc += '**`' + beautify('Utilisateurs') + '`** : ' + wbot.users.size + '\n'
+    desc += '---------------------------\n'
+    desc += '**`' + beautify('Nb devoirs serveur') + '`** : ' + nbDevoirs + '\n'
+    desc += '**`' + beautify('Nb devoirs totaux') + '`** : ' + rows[0].total + '\n'
 
-  // Envoi
-  message.channel.send(embed)
+    // Construction de la réponse
+    const embed = new Discord.RichEmbed()
+      .setColor('#3586ff')
+      .setAuthor(wbot.user.username, wbot.user.avatarURL)
+      .setTitle('Informations :')
+      .setDescription(desc)
+      .setFooter(message.author.username, message.author.avatarURL)
+      .setTimestamp()
+
+    // Envoi
+    message.channel.send(embed)
+  })
 }
 
 
