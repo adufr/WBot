12b3008dac92003cacaf18034c4d3993aeaaad5c
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-11-01 16:22:27 +0100

    opti: messages d'erreur

--- src/util/errors.js
+++ src/util/errors.js
@@ -14,9 +14,9 @@ const Discord = require('discord.js')
  * Envoie de l'embed d'erreur
  */
 function sendErrorEmbed (message, title, description) {
-  // Création de l'embed
+  // Création de l`embed
   const embed = new Discord.RichEmbed()
-    .setColor('#ff0000') // rouge
+    .setColor(`#ff0000`) // rouge
     .setTitle(title)
     .setDescription(description)
     .setFooter(message.author.username, message.author.avatarURL)
@@ -36,7 +36,7 @@ function sendErrorEmbed (message, title, description) {
  * Commande inexistante
  */
 exports.commandNotFound = (wbot, message, cmd) => {
-  sendErrorEmbed(message, 'Erreur : commande introuvable', 'La commande **' + cmd + '** est introuvable.\nFaites **!help** pour obtenir la liste des commandes.')
+  sendErrorEmbed(message, `Erreur : commande introuvable`, `La commande **${cmd}** est introuvable.\nFaites **${cmd[0]}help** pour obtenir la liste des commandes.`)
 }
 
 
@@ -44,15 +44,15 @@ exports.commandNotFound = (wbot, message, cmd) => {
  * Permission insuffisante :
  */
 exports.noPerm = (wbot, message) => {
-  sendErrorEmbed(message, 'Erreur : permission refusée', 'Vous n\'avez pas la permission d\'effectuer cette action.')
+  sendErrorEmbed(message, `Erreur : permission refusée`, `Vous n'avez pas la permission d'effectuer cette action.`)
 }
 
 
 /**
- * Impossible de trouver l'utilisateur
+ * Utilisateur non trouvé
  */
 exports.userNotFound = (wbot, message, user) => {
-  sendErrorEmbed(message, 'Erreur : utilisateur non trouvé', 'L\'utilisateur **' + user.username + '** n\'est pas sur ce serveur...')
+  sendErrorEmbed(message, `Erreur : utilisateur non trouvé`, `L'utilisateur **${user.username}** n'est pas sur ce serveur...`)
 }
 
 
@@ -60,7 +60,7 @@ exports.userNotFound = (wbot, message, user) => {
  * Rôle non trouvé
  */
 exports.roleNotFound = (wbot, message, role) => {
-  sendErrorEmbed(message, 'Erreur : role non trouvé', 'Le role **' + role + '** n\'a pas été trouvé...')
+  sendErrorEmbed(message, `Erreur : role non trouvé`, `Le role **${role}** n'a pas été trouvé...`)
 }
 
 
@@ -68,7 +68,7 @@ exports.roleNotFound = (wbot, message, role) => {
  * Rôle non défini
  */
 exports.roleNotDef = (wbot, message, str) => {
-  sendErrorEmbed(message, 'Erreur : aucun rôle défini', 'Le rôle **' + str + '** n\'est pas défini...')
+  sendErrorEmbed(message, `Erreur : aucun rôle défini`, `Le rôle **${str}** n'est pas défini...`)
 }
 
 
@@ -76,7 +76,7 @@ exports.roleNotDef = (wbot, message, str) => {
  * Channel non trouvé
  */
 exports.channelNotFound = (wbot, message, channel) => {
-  sendErrorEmbed(message, 'Erreur : channel non trouvé', 'Le channel **' + channel + '** n\'a pas été trouvé...')
+  sendErrorEmbed(message, `Erreur : channel non trouvé`, `Le channel **${channel}** n'a pas été trouvé...`)
 }
 
 
@@ -84,17 +84,17 @@ exports.channelNotFound = (wbot, message, channel) => {
  * Channel non défini
  */
 exports.channelNotDef = (wbot, message, str) => {
-  sendErrorEmbed(message, 'Erreur : aucun channel défini', 'Le channel de **' + str + '** n\'est pas défini...')
+  sendErrorEmbed(message, `Erreur : aucun channel défini`, `Le channel de **${str}** n'est pas défini...`)
 }
 
 
 /**
- * Commande max executée
+ * Commande mal executée
  * (utilisation incorrecte)
  */
 exports.errorWrongUsage = (wbot, commandName, message) => {
   const command = wbot.commands.get(commandName)
-  sendErrorEmbed(message, 'Erreur : utilisation incorrecte', 'Usage : **!' + command.help.usage + '**')
+  sendErrorEmbed(message, `Erreur : utilisation incorrecte`, `Usage : **!${command.help.usage}**`)
 }
 
 
@@ -103,7 +103,7 @@ exports.errorWrongUsage = (wbot, commandName, message) => {
  * (doit être un nombre)
  */
 exports.NaN = (wbot, message) => {
-  sendErrorEmbed(message, 'Erreur : NaN', 'Veuillez indiquer un **chiffre valide**.')
+  sendErrorEmbed(message, `Erreur : NaN`, `Veuillez indiquer un **chiffre valide**.`)
 }
 
 
@@ -112,7 +112,7 @@ exports.NaN = (wbot, message) => {
  * (non compris entre le min et le max)
  */
 exports.numberBetween = (wbot, message, min, max) => {
-  sendErrorEmbed(message, 'Erreur : nombre invalide', 'Veuillez indiquer un chiffre **entre ' + min + ' et ' + max + '**.')
+  sendErrorEmbed(message, `Erreur : nombre invalide`, `Veuillez indiquer un chiffre **entre ${min} et ${max}**.`)
 }
 
 
@@ -121,16 +121,7 @@ exports.numberBetween = (wbot, message, min, max) => {
  * (non compris entre le min et le max)
  */
 exports.argLengthBetween = (wbot, message, arg, min, max) => {
-  sendErrorEmbed(message, 'Erreur : argument invalide', 'Veuillez indiquer **' + arg + '** entre **' + min + ' et ' + max + ' caractères**.')
-}
-
-
-/**
- * Commande non disponible sur le serveur sur laquelle elle a
- * été effectuée...
- */
-exports.argLengthBetween = (wbot, message, arg, min, max) => {
-  sendErrorEmbed(message, 'Erreur : commande indisponible', 'Cette commande n\'est pas disponible sur ce serveur...')
+  sendErrorEmbed(message, `Erreur : argument invalide`, `Veuillez indiquer **${arg}** entre **${min} et ${max} caractères**.`)
 }
 
 
@@ -138,5 +129,5 @@ exports.argLengthBetween = (wbot, message, arg, min, max) => {
  * Messages privés non supportés
  */
 exports.directMessage = (wbot, message) => {
-  sendErrorEmbed(message, 'Erreur : messages privés non supportés', 'Les messages privés ne sont pas supportés.\nSi vous souhaitez vous servir du WBot, vous devez l\'inviter sur un serveur.')
+  sendErrorEmbed(message, `Erreur : messages privés non supportés`, `Les messages privés ne sont pas supportés.\nSi vous souhaitez vous servir du WBot, vous devez l'inviter sur un serveur.`)
 }
