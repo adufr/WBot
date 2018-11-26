d723dd94523a13ad5a3aea6749541340028e1cda
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-11-26 22:44:56 +0100

    ajout: affichage de l'aide par commande

--- commands/General/help.js
+++ commands/General/help.js
@@ -12,19 +12,51 @@ module.exports = class extends Command {
 
     constructor(...args) {
         super(...args, {
-            runIn: ["text"],
+            name: "help",
             cooldown: 5,
             permissionLevel: 0,
             aliases: ["commandes", "commands", "commande", "cmds", "cmd", "aides", "aide", "h"],
+            runIn: ["text", "dm"],
             description: "Affiche la liste des commandes du bot",
+            extendedHelp: "La commande help supporte 1 argument facultatif. Si aucun argument n'est renseigné, le bot renverra la liste complète des commandes disponibles. Sinon, si l'argument est une commande (ou un alias), vous obtiendrez la description complète de la commande."
         });
+        this.usageCustom = "%help <command>";
+        this.example = "%help task";
     }
 
     async run(message) {
 
+        // Récupération des arguments
+        const args = message.content.split(" ");
+
+
+        // Si le premier argument est une commande qui existe
+        // on affiche l'aide de cette commande :
+        if (this.client.commands.get(args[1])) {
+            // Récupération objet commande
+            const cmd = this.client.commands.get(args[1])
+
+            const embed = new MessageEmbed()
+                .setColor("#3586ff")
+                .setTitle(`Commande %${args[1]}`)
+                .setDescription(cmd.extendedHelp)
+                .addField("Utilisation", cmd.usageCustom, true)
+                .addField("Exemple", cmd.example, true)
+                .addField("Aliases", `\`${getAliases(cmd)}\``, false)
+                .setFooter(message.author.username, message.author.avatarURL)
+                .setTimestamp()
+            return message.channel.send(embed);;
+        }
+
+
+        // Si il y a des arguments mais qu'ils ne constituent pas une commande valide
+        if (args[1]) return message.reply(`commande **%${args[1]}** introuvable... Tapez \`%help\` pour afficher la liste des commandes.`);
+
+
+        // Si il n'y a aucun argument
+        // Affichage de la liste des commandes
         Promise.all([
             loadCommands(this, this.client, __dirname),
-
         ]).then(function (responses) {
             const embed = new MessageEmbed()
                 .setColor('#3586ff')
@@ -36,7 +68,6 @@ module.exports = class extends Command {
             message.channel.send(embed)
         }).catch(function (errors) {
             // TODO
-            message.channel.send("erreur : " + errors);
             // this.client.logger.log(errors, 'error')
         })
 
@@ -45,6 +76,11 @@ module.exports = class extends Command {
 };
 
 
+/**
+ * Fonction qui loop sur tout les commandes
+ * Renvoie une promesse string avec nom + shortDesc pour
+ * former le message d'aide..
+ */
 function loadCommands(command, client, commandsPath) {
     return new Promise(function (resolve, reject) {
         fs.readdir(commandsPath, (err, files) => { // boucle sur tout les fichiers du dossier
@@ -64,4 +100,18 @@ function loadCommands(command, client, commandsPath) {
             resolve(description)
         })
     })
+}
+
+
+/**
+ * Fonction retournant un string avec la liste
+ * des alias de la commande passée en paramètre
+ * avec un minimum de mise en forme
+ */
+function getAliases(cmd) {
+    let string = '';
+    cmd.aliases.forEach(alias => {
+        string += ', ' + alias;
+    });
+    return string.substr(2);
 }
