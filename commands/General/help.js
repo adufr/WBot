06a1b5933782fab8cec40d180db3dcb8ebd2d51d
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-12-07 09:43:41 +0100

    modif: diverses modifications sur l'embed

--- commands/General/help.js
+++ commands/General/help.js
@@ -1,12 +1,11 @@
-const fs = require('fs');
-const path = require('path');
 const {
     Command
 } = require("klasa");
 const {
     MessageEmbed
 } = require("discord.js");
-const Discord = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class extends Command {
 
@@ -15,7 +14,7 @@ module.exports = class extends Command {
             name: "help",
             cooldown: 5,
             permissionLevel: 0,
-            aliases: ["commandes", "commands", "commande", "cmds", "cmd", "aides", "aide", "h"],
+            aliases: ["commandes", "commands", "commande", "cmds", "cmd", "aide", "halp", "h"],
             runIn: ["text", "dm"],
             description: "Affiche la liste des commandes du bot",
             extendedHelp: "La commande help supporte 1 argument facultatif. Si aucun argument n'est renseigné, le bot renverra la liste complète des commandes disponibles. Sinon, si l'argument est une commande (ou un alias), vous obtiendrez la description complète de la commande."
@@ -43,8 +42,8 @@ module.exports = class extends Command {
                 .addField("Utilisation", cmd.usageCustom, true)
                 .addField("Exemple", cmd.example, true)
                 .addField("Aliases", `\`${getAliases(cmd)}\``, false)
-                .setFooter(message.author.username, message.author.avatarURL)
                 .setTimestamp()
+                .setFooter(this.client.user.username, this.client.user.displayAvatarURL());
             return message.channel.send(embed);;
         }
 
@@ -55,20 +54,22 @@ module.exports = class extends Command {
 
         // Si il n'y a aucun argument
         // Affichage de la liste des commandes
+        const client = this.client;
         Promise.all([
-            loadCommands(this, this.client, __dirname),
+            loadCommands(this, client, path.join(__dirname, "../Tasks/")),
+            loadCommands(this, client, __dirname),
         ]).then(function (responses) {
             const embed = new MessageEmbed()
                 .setColor('#3586ff')
                 .setTitle('Liste des commandes :')
-                .setDescription('Voici la liste des commandes du bot, classées par groupe de permissions')
-                .addField('Commandes utilisateurs :', responses[0])
-                .setFooter(message.author.username, message.author.avatarURL)
+                .setDescription('Voici la liste des commandes du bot, classées par catégories.')
+                .addField('Commandes calendrier :', responses[0])
+                .addField('Commandes générales :', responses[1])
                 .setTimestamp()
-            message.channel.send(embed)
+                .setFooter(client.user.username, client.user.displayAvatarURL());
+            return message.channel.send(embed);
         }).catch(function (errors) {
-            // TODO
-            // this.client.logger.log(errors, 'error')
+            throw errors;
         })
 
     }
