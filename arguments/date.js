726e3bc6a12ba9ac3ff3aa82ff7073370154f202
Woosy <dufourarthur.perso@gmail.com>
2018-12-18 14:22:19 +0100

    fix: date au format fr

--- arguments/date.js
+++ arguments/date.js
@@ -6,7 +6,8 @@ const moment = require('moment');
 module.exports = class extends Argument {
 
     run(arg, possible, message) {
-        const date = new Date(arg);
+        var date = moment(arg, "DD/MM/YY");
+        date = date.toDate();
         if (date && !isNaN(date.getTime()) && date.getTime() > Date.now()) return date;
         else throw `<@${message.author.id}>, la date indiquée n'est pas valide !`;
     }
