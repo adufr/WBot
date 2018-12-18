165b01c66e2f648f7d534ec5b9d80fc92c8fd20d
Woosy <dufourarthur.perso@gmail.com>
2018-12-18 13:13:50 +0100

    fix: enregistrement de la description de la tâche

--- commands/tasks/addtasks.js
+++ commands/tasks/addtasks.js
@@ -14,18 +14,18 @@ module.exports = class extends Command {
             permissionLevel: 0,
             aliases: ["addtasks", "addtask", "add", "at"],
             runIn: ["text"],
-            usage: "<titre:string> <due_date:string> <description:string>",
+            usage: "<titre:string> <due_date:string> <description:string>  [...]",
             usageDelim: " "
         });
         this.usageCustom = "%addtask <titre:string> <due_date:string> <description:string>";
         this.example = "%addtask Maths 28/10/18 Faire l'exercice 69 de la page 420.";
     }
 
-    async run(message, [titre, due_date, description]) {
+    async run(message, [titre, due_date, ...description]) {
         const task = {
             title: titre,
             due_date: due_date,
-            description: description
+            description: description.join(" ")
         };
         await message.guild.settings.update("tasklist.tasks", task);
         return message.reply(`votre tâche : **${titre}**, pour le **${due_date}** a bien été ajoutée ! :white_check_mark:`);
